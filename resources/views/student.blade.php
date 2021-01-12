f1fe04b1c3fe3e1d4417d6e13fe85a5d450710fc
HSIAO,KAI-FU <Tokizakimadthree@gmail.com>
2021-01-12 23:01:56 +0800

    學生點名提醒

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -16,6 +16,18 @@
                     </div>
                 </header>
                 <!-- Top Image -->
+                @if ($message = Session::get('error'))
+                    <div class="alert alert-danger alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{{ $message }}</strong>
+                    </div>
+                @endif
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{{ $message }}</strong>
+                    </div>
+                @endif
                 <section class="top-image">
                     <div class="container-fluid">
                         <div class="row">
